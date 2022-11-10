64e3fec9452055e4efe70127f92dff87c882fb10
Mustafa Tevfik Kadan <mtevfik41@gmail.com>
2022-11-10 06:52:05 +0300

    some refactor

--- shoppingservice/src/main/java/com/fiba/shoppingservice/service/entityservice/CartEntityService.java
+++ shoppingservice/src/main/java/com/fiba/shoppingservice/service/entityservice/CartEntityService.java
@@ -12,6 +12,8 @@ import com.fiba.shoppingservice.exception.exceptions.CartNotFoundException;
 import com.fiba.shoppingservice.exception.exceptions.CustomerAlreadyExistException;
 import com.fiba.shoppingservice.exception.exceptions.ProductNotFoundException;
 import com.fiba.shoppingservice.repository.CartRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -24,6 +26,7 @@ import java.util.Optional;
  */
 @Service
 public class CartEntityService {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     private final CartRepository cartRepository;
 
@@ -35,14 +38,17 @@ public class CartEntityService {
     }
 
     public CartDto saveCart(Cart cart) {
+        log.info("Cart saving process started");
         if (existByName(cart.getCustomerName())) {
+            log.error(ErrorMessage.CUSTOMER_ALREADY_EXIST.getMessage());
             throw new CustomerAlreadyExistException(ErrorMessage.CUSTOMER_ALREADY_EXIST);
         }
 
         Cart savedCart = cartRepository.save(cart);
-
+        log.info("Cart saved successfully: {}", savedCart);
         CartDto savedCartDto = CartMapper.INSTANCE.convertToCartDto(savedCart);
 
+        log.info("Cart saving process finished");
         return savedCartDto;
 
     }
@@ -52,17 +58,21 @@ public class CartEntityService {
     }
 
     public Cart getCartById(long id) {
+        log.info("Get cart by id process started");
         if (!existById(id)) {
+            log.error(ErrorMessage.CART_NOT_FOUND.getMessage());
             throw new CartNotFoundException(ErrorMessage.CART_NOT_FOUND);
         }
 
         Optional<Cart> cart = cartRepository.findById(id);
+        log.info("Cart found successfully: {}", cart.get());
 
+        log.info("Get cart by id process finished");
         return cart.get();
     }
 
     public CartDto addProductToCart(Cart cart, CartProduct cartProduct) {
-
+        log.info("Add product to cart process started");
         CartProduct savedCartProduct = cartProductEntityService.saveCartProduct(cartProduct);
 
         cart.getCartProducts().add(savedCartProduct);
@@ -70,24 +80,29 @@ public class CartEntityService {
         cart.setTotalAmount(cart.getTotalAmount().add(savedCartProduct.getLineAmount()));
 
         Cart savedCart = cartRepository.save(cart);
+        log.info("Cart saved successfully: {}", savedCart);
 
         CartDto savedCartDto = CartMapper.INSTANCE.convertToCartDto(savedCart);
 
+        log.info("Add product to cart process finished");
         return savedCartDto;
 
     }
 
     public void removeProductFromCart(Cart cart, CartProduct cartProduct) {
-
+        log.info("Remove product from cart process started");
         cartRepository.save(cart);
+        log.info("CartProduct removed from cart successfully: {}", cartProduct);
 
         cartProductEntityService.deleteCartProductById(cartProduct.getCartProductId());
-
+        log.info("Remove product from cart process finished");
     }
 
     public CartDto removeAllCartProductsAndCheckoutCart(Cart cart, List<CartProduct> cartProducts) {
+        log.info("Checkout cart process started");
 
         Cart savedCart = cartRepository.save(cart);
+        log.info("Cart saved successfully: {}", savedCart);
 
         for (CartProduct cartProduct : cartProducts) {
             cartProductEntityService.deleteCartProductById(cartProduct.getCartProductId());
@@ -95,34 +110,46 @@ public class CartEntityService {
 
         CartDto savedCartDto = CartMapper.INSTANCE.convertToCartDto(savedCart);
 
+        log.info("Checkout cart process finished");
         return savedCartDto;
     }
 
     public CartProductDto getCartProductById(long cartProductId) {
+        log.info("Get cart product by id process started");
         CartProduct cartProduct = cartProductEntityService.findCartProductById(cartProductId);
 
         CartProductDto cartProductDto = CartProductMapper.INSTANCE.convertToCartProductDto(cartProduct);
+        log.info("Cart product found successfully: {}", cartProductDto);
 
+        log.info("Get cart product by id process finished");
         return cartProductDto;
     }
 
     public ProductDto getProductById(Long id) {
+        log.info("Get product by id process started");
         try {
             String url = "http://localhost:8081/api/inventory/products/" + id;
             RestTemplate restTemplate = new RestTemplate();
             ProductDto productDto = restTemplate.getForObject(url, ProductDto.class);
+            log.info("Product found successfully: {}", productDto);
             return productDto;
         } catch (Exception e) {
+            log.error(ErrorMessage.PRODUCT_NOT_FOUND.getMessage());
             throw new ProductNotFoundException(ErrorMessage.PRODUCT_NOT_FOUND);
         }
     }
 
     public void deleteCartById(long id) {
+        log.info("Delete cart by id process started");
         if (!existById(id)) {
+            log.error(ErrorMessage.CART_NOT_FOUND.getMessage());
             throw new CartNotFoundException(ErrorMessage.CART_NOT_FOUND);
         }
 
         cartRepository.deleteById(id);
+        log.info("Cart deleted successfully: {}", id);
+
+        log.info("Delete cart by id process finished");
     }
 
     public boolean existById(long id) {
--- shoppingservice/src/main/java/com/fiba/shoppingservice/service/entityservice/CartProductEntityService.java
+++ shoppingservice/src/main/java/com/fiba/shoppingservice/service/entityservice/CartProductEntityService.java
@@ -4,6 +4,8 @@ import com.fiba.shoppingservice.entity.CartProduct;
 import com.fiba.shoppingservice.enums.ErrorMessage;
 import com.fiba.shoppingservice.exception.exceptions.CartProductNotFoundException;
 import com.fiba.shoppingservice.repository.CartProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -15,6 +17,8 @@ import java.util.Optional;
 @Service
 public class CartProductEntityService {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     private final CartProductRepository cartProductRepository;
 
     public CartProductEntityService(CartProductRepository cartProductRepository) {
@@ -23,11 +27,13 @@ public class CartProductEntityService {
 
     public CartProduct findCartProductById(Long id) {
         if (!existById(id)) {
+            log.error(ErrorMessage.CART_PRODUCT_NOT_FOUND.getMessage());
             throw new CartProductNotFoundException(ErrorMessage.CART_PRODUCT_NOT_FOUND);
         }
 
         CartProduct cartProduct = cartProductRepository.findById(id).get();
 
+        log.info("CartProduct found by id: " + id);
         return cartProduct;
     }
 
@@ -38,6 +44,7 @@ public class CartProductEntityService {
     public CartProduct saveCartProduct(CartProduct cartProduct) {
         CartProduct savedCartProduct = cartProductRepository.save(cartProduct);
 
+        log.info("CartProduct saved cart successfully: {}", savedCartProduct);
         return savedCartProduct;
     }
 
@@ -45,13 +52,21 @@ public class CartProductEntityService {
         Optional<CartProduct> cartProduct = cartProductRepository.findCartProductByCartIdAndCartProductId(cartId, cartProductId);
 
         if (!cartProduct.isPresent()) {
+            log.error(ErrorMessage.CART_PRODUCT_NOT_FOUND.getMessage());
             throw new CartProductNotFoundException(ErrorMessage.CART_PRODUCT_NOT_FOUND);
         }
 
+        log.info("CartProduct found successfully: {}", cartProduct.get());
         return cartProduct.get();
     }
 
     public void deleteCartProductById(Long cartProductId) {
+        if (!existById(cartProductId)) {
+            log.error(ErrorMessage.CART_PRODUCT_NOT_FOUND.getMessage());
+            throw new CartProductNotFoundException(ErrorMessage.CART_PRODUCT_NOT_FOUND);
+        }
+
+        log.info("CartProduct deleted successfully: {}", cartProductId);
         cartProductRepository.deleteById(cartProductId);
     }
 }
